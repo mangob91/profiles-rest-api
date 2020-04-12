b17d52476faf887f0c2b75c624f0f01c097a37a1
mangob91 <yl3794@columbia.edu>
2020-04-12 17:09:40 -0500

    Registering new model

--- profiles_api/admin.py
+++ profiles_api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from profiles_api import models
+
 # Register your models here.
+"""Registering model will let model to appear under django administration view"""
+admin.site.register(models.UserProfile)
